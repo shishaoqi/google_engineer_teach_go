27035dbf1ad9fcdc559e7f2d86560c099f323d32
shizhongqi <shishaoqi0928@gmail.com>
2023-10-18 12:39:13 +0800

    反射知识

--- goLanguage/basic/type/conver.go
+++ goLanguage/basic/type/conver.go
@@ -3,12 +3,25 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"sync"
 )
+
+type atomicInt2 struct {
+	value int
+	lock sync.Mutex
+}
 
 func main() {
 	var p interface{}
 
 	p = 21
 
-	fmt.Println(reflect.TypeOf(p))
+	typeOfp := reflect.TypeOf(p)
+	fmt.Println(typeOfp)
+
+	fmt.Println(typeOfp.Name(), typeOfp.Kind())
+
+	var a atomicInt2
+	typeOfA := reflect.TypeOf(a)
+	fmt.Println(typeOfA.Name(), typeOfA.Kind())
 }
